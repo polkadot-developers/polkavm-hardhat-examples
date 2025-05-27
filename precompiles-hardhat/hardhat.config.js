bb64bab3012f890666555a117c379bf7ce38d17f
nhussein11 <nicolashussein14@gmail.com>
2025-05-27 10:25:09 -0300

    fix: preocompile readme

--- precompiles-hardhat/hardhat.config.js
+++ precompiles-hardhat/hardhat.config.js
@@ -1,22 +1,22 @@
-require('@nomicfoundation/hardhat-toolbox');
-require('@parity/hardhat-polkadot');
+require("@nomicfoundation/hardhat-toolbox");
+require("@parity/hardhat-polkadot");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: '0.8.28',
-    resolc: {
-        compilerSource: 'npm',
+  solidity: "0.8.28",
+  resolc: {
+    compilerSource: "npm",
+  },
+  networks: {
+    hardhat: {
+      polkavm: true,
+      forking: {
+        url: "wss://westend-asset-hub-rpc.polkadot.io",
+      },
+      adapterConfig: {
+        adapterBinaryPath: "INSERT_PATH_TO_ETH_RPC_ADAPTER",
+        dev: true,
+      },
     },
-    networks: {
-        hardhat: {
-            polkavm: true,
-            forking: {
-                url: 'wss://westend-asset-hub-rpc.polkadot.io',
-            },
-            adapterConfig: {
-                adapterBinaryPath: '../bin/eth-rpc',
-                dev: true,
-            },
-        },
-    }
+  },
 };
